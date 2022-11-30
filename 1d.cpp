07c8f8d465161606441b91de461b1ed8e8a9a541
kenkomu <komukenneth216@gmail.com>
2022-11-30 20:47:10 +0300

    1d.cpp

--- 1d.cpp
+++ 1d.cpp
@@ -1,46 +1,20 @@
-//CLASS CONSTRUCTOR
 #include <iostream>
 using namespace std;
 class MyClass {
   public:
-    MyClass() {
-        cout << "Hello World!";
+    MyClass() { //CLASS CONSTRUCTOR
+        cout << "Hello World!"<<endl;
     }
-};
-
-int main() {
-  MyClass myObj;
-  return 0;
-}
-//CLASS DESTRUCTOR
-
-#include <iostream>
-using namespace std;
-class MyClass {
-  public:
-    ~MyClass() {
-        cout << "Hello World!";
+    ~MyClass() { //CLASS DESTRUCTOR
+        cout << "Hello World!"<<endl;
     }
+    friend int Age(int x); //FRIEND FUNCTION
 };
-
-int main() {
-  MyClass myObj;
-  return 0;
-}
-
-// FRIEND FUNCTION
-#include <iostream>
-using namespace std;
-class Animal{
-    public:
-        friend int Age(int x);
-};
-int Age(int x){
+    int Age(int x){
             cout << "Animal's age is "<< x << endl;
-        }
+    }
 int main() {
-  Animal lion;
+  MyClass myObj;
   Age(10);
-  //cout<<Age(10) << endl;
   return 0;
 }
